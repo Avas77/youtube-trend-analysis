5145235c28845ad82bd1f7e8bb39daf941bca715
Avas <insanebuzz09@gmail.com>
2025-05-15 09:12:52 -0400

    ingested data from youtube and loaded into sql

---
+++ postgres_setup.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Replace with your actual PostgreSQL credentials
+DB_USER = 'postgres'
+DB_PASS = 'postgres'
+DB_HOST = 'localhost'
+DB_PORT = '5432'
+DB_NAME = 'youtube_data'
+
+conn_str = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+engine = create_engine(conn_str)
+
+
+df = pd.read_csv('dataset/trending_videos_US_20250511_100254.csv')
+# Ensure correct column types for PostgreSQL
+df['published_at'] = pd.to_datetime(df['published_at'])
+
+# Write to PostgreSQL (table will be created if it doesn't exist)
+table_name = 'trending_videos'
+
+df.to_sql(table_name, con=engine, if_exists='append', index=False)
+
+print(f"Data inserted into PostgreSQL table: {table_name}")
---
+++ youtube_data_ingestion.py
@@ -0,0 +1,47 @@
+from googleapiclient.discovery import build
+import pandas as pd
+from datetime import datetime
+
+
+# --- CONFIGURATION ---
+API_KEY = 'AIzaSyCmBl5YX200cdYkxKjy7QOhzq13GFeNugY'
+REGION = 'US'  # Change as needed
+MAX_RESULTS = 50  # API max per call
+OUTPUT_FORMAT = 'csv'
+
+# --- YOUTUBE API CLIENT ---
+youtube = build('youtube', 'v3', developerKey=API_KEY)
+
+# --- API CALL ---
+request = youtube.videos().list(
+    part="snippet,statistics",
+    chart="mostPopular",
+    regionCode=REGION,
+    maxResults=MAX_RESULTS
+)
+
+response = request.execute()
+
+video_data = []
+
+for item in response['items']:
+    video_info = {
+        'video_id': item['id'],
+        'title': item['snippet']['title'],
+        'channel': item['snippet']['channelTitle'],
+        'published_at': item['snippet']['publishedAt'],
+        'category_id': item['snippet']['categoryId'],
+        'view_count': int(item['statistics'].get('viewCount', 0)),
+        'like_count': int(item['statistics'].get('likeCount', 0)),
+        'comment_count': int(item['statistics'].get('commentCount', 0))
+    }
+    video_data.append(video_info)
+
+df = pd.DataFrame(video_data)
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+filename = f"trending_videos_{REGION}_{timestamp}.csv"
+df.to_csv(filename, index=False)
+
+print(f"Data saved to {filename}")
+
